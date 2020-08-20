fcdabfb5f7db108ac4f3d589265acc2e05517d1b
刘群 <517067180@qq.com>
2020-08-20 11:27:37 +0800

    init helloworld

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,13 @@
+// helloworld.cpp : 定义控制台应用程序的入口点。
+//
+
+//#include "stdafx.h"
+
+#include <stdio.h>
+#include <tchar.h>
+int _tmain(int argc, _TCHAR* argv[])
+{
+	puts("Hello world!\n");
+	return 0;
+}
+
